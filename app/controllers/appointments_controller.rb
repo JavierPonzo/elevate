f2f2b1278402921e32e8f7d95056b9faeff674cf
Johnny06-coder <jmaradiaga06@gmail.com>
2024-12-10 13:24:07 -0600

    appointment y question&answer controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,16 +1,42 @@
 class AppointmentsController < ApplicationController
   def index
+    if current_user.doctor?
+      @appointments = Appointment.where(doctor_id: current_user.doctor.id)
+    else
+      @appointments = current_user.appointments
+    end
   end
 
   def new
+    @appointment = Appointment.new
+    @doctors = Doctor.all
   end
 
   def show
+    @appointment = current_user.appointment.find(params[id])
   end
 
   def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user = current_user
+    if @appointment.save
+      redirect_to appointments_path, notice: 'Your appointment has succesfully been created'
+    else
+      @doctors = Doctor.all
+      render :new , alert: 'Your appointment has failed to be created, please double check your information.'
+    end
   end
 
   def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to appointments_path, notice: "Your appointment has succesfully been deleted"
+  end
+
+
+  private  
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :status, :details, :doctor_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :posts
+  resources :posts do 
+    resources :question_answers, only: [:index, :new, :create]
+  end
   resources :appointments, only: [:index, :new, :show, :create, :destroy]
-  resources :question_answers, only: [:index, :new, :create]
   resources :reviews, only: [:create, :destroy]
   # Defines the root path route ("/")
   # root "posts#index"

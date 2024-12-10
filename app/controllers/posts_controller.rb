f433bdb077042efa6b3d66c67fb2df683b3ffe8a
Joaquin Bernardini <bernardinijoaquin@gmail.com>
2024-12-10 21:26:48 +0000

    create-edit-forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,23 +10,34 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def new
     @post = Post.new
+    @post.doctor_id = current_user.doctor&.id
   end
 
   def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to post_path(@post)
+    if current_user.doctor?
+      @post = Post.new(post_params)
+      @post.doctor_id = current_user.doctor&.id
+      if @post.save
+        redirect_to post_path(@post)
+      else
+        render :new, status: :unprocessable_entity
+      end
     else
-      render :new, status: :unprocessable_entity
+      redirect_to posts_path, alert: "Solo los doctores puden crear posts"
     end
   end
 
   def edit
+    redirect_to post_path, alert: "No tenes permiso para editar este articulo." unless @post.doctor_id == current_user.doctor&.id
   end
 
   def update
-    @post.update
-    redirect_to post_path(@post)
+    @post.doctor_id = current_user.doctor&.id
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -41,6 +52,6 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   end
 
   def post_params
-    params.require(:post).permit(:title, :category, :doctor_id, :content, photos: [])
+    params.require(:post).permit(:title, :category, :content, photos: [])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :doctor
+  belongs_to :doctor, optional: true
   has_many :question_answers
   has_many :reviews
   has_many_attached :photos

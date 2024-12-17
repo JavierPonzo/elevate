e2551015f0a6ca7e7a3b7cae728e06d87e856923
Joaquin Bernardini <bernardinijoaquin@gmail.com>
2024-12-17 21:28:04 +0000

    search-bar

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -5,29 +5,38 @@ export default class extends Controller {
   static targets = ["input", "list"]
 
   connect() {
-    //console.log(this.inputTarget)
-    //console.log(this.listTarget)
+    // Esto se ejecuta cuando el controlador se conecta
   }
 
   suggestions() {
     const query = this.inputTarget.value
     const url = `/posts/search_suggestions?query=${query}`
-    fetch(url,
-     { headers: {
+
+    if (query.length < 2) {
+      this.listTarget.innerHTML = ''
+      this.listTarget.style.display = 'none'
+      return
+    }
+
+    fetch(url, {
+      headers: {
         "Accept": "application/json"
-      }}
-    )
-    .then(response=>response.json())
-    .then(data=>this.updateList(data))
+      }
+    })
+    .then(response => response.json())
+    .then(data => this.updateList(data))
   }
 
   updateList(suggestions) {
-    this.listTarget.innerHTML=""
-
-    suggestions.forEach((suggestion) => {
-      const li = `<li>${suggestion}</li>`
-      this.listTarget.insertAdjacentHTML("beforeend", li)
-    })
+    this.listTarget.innerHTML = ""
+    if (suggestions.length === 0) {
+      this.listTarget.style.display = 'none'
+    } else {
+      suggestions.forEach((suggestion) => {
+        const li = `<li class="list-group-item"><a href="/posts?query=${encodeURIComponent(suggestion)}">${suggestion}</a></li>`
+        this.listTarget.insertAdjacentHTML("beforeend", li)
+      })
+      this.listTarget.style.display = 'block'
+    }
   }
-
 }

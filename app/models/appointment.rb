6cdc5afe68ba9da9885f80cc926e08069addaff5
Javier Ponzo <ponzojavier@gmail.com>
2024-12-16 21:41:05 -0400

    fixing bugs

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,11 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
+  validates :date, presence: true
+  validates :rating, presence: true
 
   private
-  
+
   def set_default_status
     self.status ||= "Pending"
   end

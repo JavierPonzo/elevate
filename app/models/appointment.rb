017aa036975810e65fcf7e1b514a3ecc81228edd
Javier Ponzo <ponzojavier@gmail.com>
2024-12-17 14:49:45 -0400

    master asked me to commit changes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,8 +2,6 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
   validates :date, presence: true
-  validates :rating, presence: true
-
   before_save :set_default_status
 
   validates :status, inclusion: { in: ["Pendiente", "Confirmado", "Cancelado"] }
@@ -11,6 +9,6 @@ class Appointment < ApplicationRecord
   private
 
   def set_default_status
-    self.status ||= "Pendiente"
+    self.status = "Pendiente"
   end
 end

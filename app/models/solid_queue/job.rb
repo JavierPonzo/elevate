e8a91ac8d0c08d818ee55c22d272c75b9b390781
Javier Ponzo <ponzojavier@gmail.com>
2024-12-28 17:26:47 +0100

    fixes seb

--- app/models/solid_queue/job.rb
+++
@@ -1,5 +0,0 @@
-module SolidQueue
-  class Job < SolidQueue::Record
-    self.table_name = "solid_queue_jobs"
-  end
-end
--- app/models/solid_queue/record.rb
+++
@@ -1,6 +0,0 @@
-module SolidQueue
-  class Record < ApplicationRecord
-    self.abstract_class = true
-    connects_to database: { writing: :queue }
-  end
-end

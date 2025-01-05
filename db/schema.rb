886464c3eaa2e91e03755c53381dc22de80d21e7
Javier Ponzo <ponzojavier@gmail.com>
2025-01-05 14:42:01 +0100

    added time, fixed issues with appointments and avatar size issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_04_141052) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_05_115245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "vector"
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_04_141052) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "paid"
+    t.time "time"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end

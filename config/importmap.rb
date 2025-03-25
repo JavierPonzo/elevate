3ced1d851d491a944a171dbd5417522bd86ef646
Javier Ponzo <ponzojavier@gmail.com>
2025-03-25 11:04:32 +0100

    Added action text migration

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,5 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "star-rating.js" # @4.3.1
 pin "trix", to: "https://ga.jspm.io/npm:trix@2.1.13/dist/trix.esm.min.js"
 pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@8.0.200/app/assets/javascripts/actiontext.esm.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
